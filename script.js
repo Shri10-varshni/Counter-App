d437522adbcd40ea8dcfa2d489ade318661b292f
Shri Varshni <146243128+Shri10-varshni@users.noreply.github.com>
2024-06-29 10:12:19 +0530

    Updated script.js - Improvement in Responsiveness
    Added a new function called updateButtonLabels so that the text inside the button adjusts according to the screen size

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 function incrementor() {
-    let count = document.getElementById("my-count")
-    count.innerText = Number(count.innerText)+1
+    let count = document.getElementById("my-count");
+    count.innerText = Number(count.innerText) + 1;
 }
 
 function decrementor() {
-    let count = document.getElementById("my-count")
-    count.innerText = Number(count.innerText)-1
+    let count = document.getElementById("my-count");
+    count.innerText = Number(count.innerText) - 1;
 }
 
 function saver() {
-    let saved = document.getElementById("prev-saved")
-    let count = document.getElementById("my-count")
+    let saved = document.getElementById("prev-saved");
+    let count = document.getElementById("my-count");
     if (saved.textContent == "Saved Counts: None") {
-        saved.textContent = "Saved Counts: " + count.innerText
+        saved.textContent = "Saved Counts: " + count.innerText;
+    } else {
+        saved.textContent += " - " + count.innerText;
     }
-    else(saved.textContent += " - " + count.innerText)
-        
 }
+
+function updateButtonLabels() {
+    const decButton = document.getElementById("dec-btn");
+    const incButton = document.getElementById("inc-btn");
+    if (window.innerWidth <= 600) {
+        decButton.textContent = "Dec";
+        incButton.textContent = "Inc";
+    } else {
+        decButton.textContent = "Decrement";
+        incButton.textContent = "Increment";
+    }
+}
+
+updateButtonLabels();
+
+window.addEventListener("resize", updateButtonLabels);
